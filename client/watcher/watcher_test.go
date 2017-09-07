6b52678aaf51014a0ab422b6e88e0a74d2767e93
Rob Sayre <sayre@vsco.co>
2017-09-07 15:11:42 -0700

    Change the way we track this event.

--- client/watcher/watcher_test.go
+++ client/watcher/watcher_test.go
@@ -29,10 +29,15 @@ func TestNewWatcher(t *testing.T) {
 	assert.NoError(t, err)
 
 	doneChan := make(chan bool)
+	once := sync.Once
+	closeChan := func() {
+		once.Do(func() { close(doneChan) })
+	}
+
 	w.Register(func(bts []byte) {
 		// check updated bytes on write
 		assert.Equal(t, fmt.Sprintf("%s", updatedBytes), fmt.Sprintf("%s", bts))
-		close(doneChan)
+		closeChan()
 	})
 
 	go w.Watch()
